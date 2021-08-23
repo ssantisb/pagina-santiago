bb838e70e60c052159ca58bd45d067ca7d92415b
ssantisb <89368994+ssantisb@users.noreply.github.com>
2021-08-23 00:03:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+})
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.lenguajes', {delay: 500 });
+ScrollReveal().reveal('.banner', {delay: 500 });
+ScrollReveal().reveal('.banner2', {delay: 500 });
+
